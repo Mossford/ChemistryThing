2f6ea4eb12f862c64d4aa29e858aca2675aa4107
Mosseelight <doctorphillip233@gmail.com>
2024-03-27 23:31:33 -0400

    Add project files.

---
+++ ChemistryThing/Elements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChemistryThing
+{
+
+    public static class Elements
+    {
+
+        //enum for the index to the data array
+        enum Names
+        {
+            Hydrogen,
+            Helium,
+            
+        }
+
+        //array for the data
+        public static Element[] data = 
+            {
+                //Hydrogen will be positive in this case
+                new("Hydrogen", 1, false),
+                new("Helium", 0, false),
+            };
+    }
+
+    public class Element
+    {
+        public string name { get; set; }
+        public int charge { get; set; }
+        public bool isMetal { get; set; }
+
+        public Element(string name, int charge, bool isMetal)
+        {
+            this.name = name;
+            this.charge = charge;
+            this.isMetal = isMetal;
+        }
+    }
+}
---
+++ ChemistryThing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChemistryThing
+{
+    public static class Program
+    {
+        //Made to help study for Chemistry
+
+
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
